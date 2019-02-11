584f53c7bc311b95f7a177f95f2e7aa0b895132d
takayuki.kayawari <kayamelo@gmail.com>
2019-02-11 17:36:01 +0900

    試しに金閣寺の画像のcomponentを置いてみる

---
+++ components/Kinkakuji.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="kinkakuji-wrapper">
+      <img src="/kinkakuji.jpg">
+  </div>
+</template>
+
+<style>
+  .kinkakuji-wrapper > img {
+    width: 500px;
+    padding: 5px;
+    border: #7f828b solid 1px;
+  }
+</style>
