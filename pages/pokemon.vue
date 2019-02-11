a8dd777d8ccf30b2297a3ca1911bca8d2dcc538d
takayuki.kayawari <kayamelo@gmail.com>
2019-02-11 20:53:15 +0900

    promiseのエラーハンドリングを追加

--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -6,12 +6,15 @@
 import axios from 'axios'
 
 export default {
-  asyncData ({ params }) {
+  asyncData ({ params, error }) {
     return axios.get(`https://pokeapi.co/api/v2/pokemon-species/25`)
       .then((res) => {
         // pokemon apiを使ってみる
         // NOTE: https://pokeapi.co
         return { title: res.data.flavor_text_entries[1].flavor_text }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'ページが見つかりません' })
       })
   }
 }
